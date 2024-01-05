9b7e9c4dd3c2cdcd1ec7866f320862cc4aa19dfc
liviansousas <liviansousas3@gmai.com>
2024-01-05 19:22:24 -0300

    criando projeto

---
+++ Domain/Interfaces/IVeiculoService.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Interfaces
+{
+    public interface IVeiculoService
+    {
+        //A interface é um contrato, apenas usamos para adicionar 
+        //metodos,
+        //não é feita implementação de nada 
+
+        Task PostAsync(VeiculoCommand command);
+        void PostAsync();
+        void GetAsync(); 
+    }
+}
---
+++ Services/Service/VeiculoService.cs
@@ -0,0 +1,34 @@
+﻿using Domain.Commands;
+using Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Service
+{
+    public class VeiculoService : IVeiculoService
+    {
+        public void GetAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task PostAsync(VeiculoCommand command)
+
+        {   //Toso
+            //Incluir validaçãp. só podem castrar veículos com
+            //até 5 anos de uso
+            //To do 
+            //Incluir Somente carros do tipo SUV, Sedan  e Hatch 
+            command.
+            throw new NotImplementedException();
+        }
+
+        public void PostAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
